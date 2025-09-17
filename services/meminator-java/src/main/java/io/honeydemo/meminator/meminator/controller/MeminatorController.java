25f62cf6bb780dbf2aa3636c145cd49ecdbc7d00
ianhealy91 <95576826+ianhealy91@users.noreply.github.com>
2025-09-17 11:27:46 +0000

    Commit

--- services/meminator-java/src/main/java/io/honeydemo/meminator/meminator/controller/MeminatorController.java
+++ services/meminator-java/src/main/java/io/honeydemo/meminator/meminator/controller/MeminatorController.java
@@ -20,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 @RestController
 public class MeminatorController {
@@ -124,7 +125,7 @@ public class MeminatorController {
             this.imageUrl = imageUrl;
         }
     }
-
+    @WithSpan
     private int runConvertCommand(File inputFile, String phrase, String outputFilePath) throws InterruptedException, IOException {
 
           //  Span subprocessSpan = GlobalOpenTelemetry.getTracer("pictureController").spanBuilder("convert").startSpan();
